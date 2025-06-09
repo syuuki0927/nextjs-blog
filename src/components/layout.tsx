3603576410a54619c727ae80cdad7dabb60e3fcf
Yuki Sato <yuki.sato@livesense.co.jp>
2025-06-09 19:50:18 +0900

    styleの適用

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import styles from "./layout.module.css";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <h1>Headder</h1>
+      <h1 className={styles.header1}>Headder</h1>
       {children}
-      <h1>Footer</h1>
+      <h1 className={styles.header1}>Footer</h1>
     </>
   );
 };
