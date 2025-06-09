268d14ee3d332d1638f86b22983692f4a60ba5d0
Yuki Sato <yuki.sato@livesense.co.jp>
2025-06-09 19:35:56 +0900

    画像の追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,45 +28,12 @@ export default function Home() {
       >
         <main className={styles.main}>
           <Image
-            className={styles.logo}
-            src="https://nextjs.org/icons/next.svg"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
+            src="/IMG_3581.jpeg"
+            height={144}
+            width={144}
+            alt="Avator Image"
           />
-          <ol>
-            <li>
-              Get started by editing <code>src/pages/index.tsx</code>.
-            </li>
-            <li>Save and see your changes instantly.</li>
-          </ol>
-
-          <div className={styles.ctas}>
-            <a
-              className={styles.primary}
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className={styles.logo}
-                src="https://nextjs.org/icons/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
-              Deploy now
-            </a>
-            <a
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.secondary}
-            >
-              Read our docs
-            </a>
-          </div>
+          I am syuuki!
         </main>
         <footer className={styles.footer}>
           <a
