40606d826a88b38a9ef0037e27fb052db8e0dfe2
Yuki Sato <yuki.sato@livesense.co.jp>
2025-06-11 15:57:02 +0900

    ファイル名を変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home() {
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
         <main className={styles.main}>
-          <Link href="/Home">Home</Link>
+          <Link href="/home">Home</Link>
         </main>
       </div>
     </>
